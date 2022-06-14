4f5deabfc925faa01ddae0c5cde83eaec39ec981
youyu <youyu@heytea.com>
2022-06-14 23:48:27 +0800

    工厂

---
+++ src/main/java/org/example/factory/FactoryMain.java
@@ -0,0 +1,12 @@
+package org.example.factory;
+
+/**
+ * @author yuyou
+ * @since 2022/6/14 23:16
+ */
+public class FactoryMain {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/example/factory/People.java
@@ -0,0 +1,9 @@
+package org.example.factory;
+
+/**
+ * @author yuyou
+ * @since 2022/6/14 23:32
+ */
+public class People {
+    private Integer sex;
+}
