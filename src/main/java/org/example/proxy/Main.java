5b35b4ea399a6c370c2b4359c57208e0eaeb63dc
youyu <youyu@heytea.com>
2022-06-17 18:42:53 +0800

    代理模式

--- src/main/java/org/example/proxy/Main.java
+++ src/main/java/org/example/proxy/Main.java
@@ -1,5 +1,7 @@
 package org.example.proxy;
 
+import java.lang.reflect.Proxy;
+
 /**
  * @author yuyou
  * @since 2022/6/17 18:35
@@ -8,7 +10,13 @@ public class Main {
 
     public static void main(String[] args) {
         IWorkService workService = new WorkServiceImpl();
-        IWorkService workProxy = (IWorkService) new ProxyFactory(workService).getProxy();
+        IWorkService workProxy = (IWorkService) Proxy.newProxyInstance(workService.getClass().getClassLoader(),workService.getClass().getInterfaces(),(proxy, method, args1) -> {
+            System.out.println("开始代理");
+            System.out.println("代理方法" + method.getName());
+            Object result = method.invoke(workService, args);
+            System.out.println("结束代理");
+            return result;
+        });
         workProxy.doWork();
         System.out.println("===========");
         workProxy.doRest();
