13abbf252993d677dfaa49cc7c34fb410e2174d5
youyu <youyu@heytea.com>
2022-06-23 15:56:56 +0800

    代理模式

--- src/main/java/org/example/proxy/Main.java
+++ src/main/java/org/example/proxy/Main.java
@@ -1,7 +1,5 @@
 package org.example.proxy;
 
-import java.lang.reflect.Proxy;
-
 /**
  * @author yuyou
  * @since 2022/6/17 18:35
@@ -9,16 +7,15 @@ import java.lang.reflect.Proxy;
 public class Main {
 
     public static void main(String[] args) {
+//        IWorkService workService = new WorkServiceImpl();
+//        IWorkService workProxy = (IWorkService) new ProxyFactory(workService).getProxy();
+//        workProxy.doWork();
+//        System.out.println("===========");
+//        workProxy.doRest();
         IWorkService workService = new WorkServiceImpl();
-        IWorkService workProxy = (IWorkService) Proxy.newProxyInstance(workService.getClass().getClassLoader(),workService.getClass().getInterfaces(),(proxy, method, args1) -> {
-            System.out.println("开始代理");
-            System.out.println("代理方法" + method.getName());
-            Object result = method.invoke(workService, args);
-            System.out.println("结束代理");
-            return result;
-        });
+        IWorkService workProxy = (IWorkService) new ProxyFactory(workService).getProxyCglib();
         workProxy.doWork();
-        System.out.println("===========");
+        System.out.println("=============");
         workProxy.doRest();
     }
 }
--- src/main/java/org/example/proxy/ProxyFactory.java
+++ src/main/java/org/example/proxy/ProxyFactory.java
@@ -1,12 +1,17 @@
 package org.example.proxy;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
  * @author yuyou
  * @since 2022/6/17 18:29
  */
-public class ProxyFactory {
+public class ProxyFactory implements MethodInterceptor {
 
     private Object target;
 
@@ -22,5 +27,20 @@ public class ProxyFactory {
             System.out.println("结束代理");
             return result;
         });
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("cglib代理开始");
+        Object object = methodProxy.invoke(target, objects);
+        System.out.println("cglib代理结束");
+        return object;
+    }
+
+    public Object getProxyCglib() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setCallback(this);
+        enhancer.setSuperclass(target.getClass());
+        return enhancer.create();
     }
 }
